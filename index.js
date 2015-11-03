2e3c653c1687638f3534c494a0272822a4a24f88
Daniel Harding <daniel.harding@actimo.com>
2015-11-03 15:47:46 +0100

    first (and last) commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var contentDisposition = require('content-disposition');
+var finalhandler = require('finalhandler');
+var http = require('http');
+var serveStatic = require('serve-static');
+
+// Serve up files folder
+var serve = serveStatic('files', {
+  'index': false,
+  'setHeaders': setHeaders
+});
+
+// Set header to force download
+function setHeaders(res, path) {
+  res.setHeader('Content-Disposition', contentDisposition(path));
+}
+
+// Create server
+var server = http.createServer(function(req, res){
+  var done = finalhandler(req, res);
+  serve(req, res, done);
+});
+
+// Listen
+server.listen(3000);
