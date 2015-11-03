6f23ae35a5b485de1e8e9feccbfc520dc0b7f9de
Daniel Harding <daniel.harding@actimo.com>
2015-11-03 16:01:53 +0100

    Very last commit

---
+++ normal.js
@@ -0,0 +1,18 @@
+var contentDisposition = require('content-disposition');
+var finalhandler = require('finalhandler');
+var http = require('http');
+var serveStatic = require('serve-static');
+
+// Serve up files folder
+var serve = serveStatic('files', {
+  'index': false
+});
+
+// Create server
+var server = http.createServer(function(req, res){
+  var done = finalhandler(req, res);
+  serve(req, res, done);
+});
+
+// Listen
+server.listen(3000);
